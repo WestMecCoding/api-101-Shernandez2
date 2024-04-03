961a2866f6f76f924d8efed429eb49207d38b400
Shernandez2 <142362712+Shernandez2@users.noreply.github.com>
2024-04-03 00:28:28 +0000

    end of 4/2/2024

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// const cors = require('cors')
 const { get } = require('http');
 const app = express();
 const port = 3000;
-
+// app.use(cors)
 app.get('/', (req, res) => {
-    res.send('hello world');
+    // res.send('hello world');
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.get('/csv-data', (req, res) => {
+    const cvsPath = path.join(path.join(__dirname, "..", 'data', 'zonal_sea_averages.csv'));
+    fs.readFile(cvsPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+        res.type('text/plain');
+        res.send(data);
+
+    });
 });
 
 
